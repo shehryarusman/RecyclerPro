abd0a50b40890deaa0851fb15c75c5b8efea5ad1
su83 <su83@drexel.edu>
2023-04-29 11:30:08 -0400

    added auth functionality for flask

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,95 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
+import flask
+from flask import jsonify
+import os
+import flask_sqlalchemy
+import flask_praetorian
+import flask_cors
 import pandas as pd
 import numpy as np
 import random
 import pickle
 import json
 
-app = Flask(__name__)
-CORS(app)
+
+db = flask_sqlalchemy.SQLAlchemy()
+guard = flask_praetorian.Praetorian()
+cors = flask_cors.CORS()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.Text, unique=True)
+    password = db.Column(db.Text)
+    roles = db.Column(db.Text)
+    is_active = db.Column(db.Boolean, default=True, server_default="true")
+
+    @property
+    def rolenames(self):
+        try:
+            return self.roles.split(",")
+        except Exception:
+            return []
+    
+    @classmethod
+    def lookup(cls, username):
+        return cls.query.filter_by(username=username).one_or_none()
+    
+    @classmethod
+    def identify(cls, id):
+        return cls.query.get(id)
+    @property
+    def identity(self):
+        return self.id
+    @property
+    def is_active(self):
+        return self.is_active
+
+app = flask.Flask(__name__)
+app.debug=True
+app.config['SECRET_KEY'] = 'top secret'
+app.config['JWT_ACCESS_LIFESPAN'] = {'hours': 24}
+app.config['JWT_REFRESH_LIFESPAN'] = {'days': 30}
+
+guard.init_app(app, User)
+
+#initialize a local database
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(os.getcwd(),'database.db')}"
+db.init_app(app)
+cors.init_app(app)
+
+#add users
+with app.app_context():
+    db.create_all()
+    if db.session.query(User).filter_by(username='admin').count()<1:
+        db.session.add(User(
+            username='admin',
+            password=guard.hash_password('admin'),
+            roles='admin'
+        ))
+    db.session.commit()
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    req = flask.request.get_json(force=True)
+    username = req.get('username', None)
+    password = req.get('password', None)
+    user = guard.authenticate(username, password)
+    ret = {'access_token': guard.encode_jwt_token(user)}
+    return ret, 200
+
+  
+@app.route('/api/refresh', methods=['POST'])
+def refresh():
+    print("refresh request")
+    old_token = flask.request.get_data()
+    new_token = guard.refresh_jwt_token(old_token)
+    ret = {'access_token': new_token}
+    return ret, 200
+  
+  
+@app.route('/api/protected')
+@flask_praetorian.auth_required
+def protected():
+    return {'message': f'protected endpoint (allowed user {flask_praetorian.current_user().username})'}
 
 @app.route('/')
 def index():
