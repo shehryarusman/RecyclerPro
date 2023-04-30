580cf81c820b92a6ef20f58debdcfd0db44e8c76
Alex Zavalny <az548@drexel.edu>
2023-04-30 12:45:11 -0400

    bounding boxes

--- src/component/draw.js
+++ src/component/draw.js
@@ -6,25 +6,25 @@ export const drawRect = (detections, ctx) =>{
     const [x, y, width, height] = prediction['bbox']; 
     let text = prediction['class'];
     const score = prediction['score'];
-    let color = '';
+    let color = 'blue';
     const recycle = ['bottle', 'wine glass', 'cup', 'fork','knife', 'spoon', 'bowl', 'vase','toothbrush'];
     const trash = ['banana', 'apple'];
     if(recycle.indexOf(text) > -1){
-      color = 'green';
+      color = 'blue';
       text += " "+ Math.round((score*100) * 100) / 100 + "%";
       text = "(Recylable) " + text;
     // Set styling
-    ctx.strokeStyle = '#' + color
+    ctx.strokeStyle = color
     ctx.font = '18px Arial';
 
     // Draw rectangles and text
     ctx.beginPath();   
-    ctx.fillStyle = '#' + color
+    ctx.fillStyle = color
     ctx.fillText(text, x, y);
     ctx.rect(x, y, width, height); 
     ctx.stroke();
     } else if (trash.indexOf(text) > -1){
-      color='red';
+      //color='red';
       text += " "+ Math.round((score*100) * 100) / 100 + "%";
       text = "(Trash) " + text;
     // Set styling
